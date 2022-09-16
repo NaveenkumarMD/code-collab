2821c31e2ef4cfbe892c1066346a02cfea5be914
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-09-16 10:57:01 +0530

    scripts configured

--- Backend/index.js
+++ Backend/index.js
@@ -4,8 +4,10 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-const PORT = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 5000
+app.get("/",(req,res)=>{
+    res.send("fuck")
+})
 app.listen(PORT, () => {
     console.log("App is running at", PORT)
 })
