80de7782488e87b233f92c6d7cfaf212e2194827
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-09-24 19:48:34 +0530

    editor accessory buttons and their functions

--- frontend/src/Components/Editor.jsx
+++ frontend/src/Components/Editor.jsx
@@ -1,66 +1,150 @@
 import React, { useRef, useState } from 'react'
-import Editor from "@monaco-editor/react";
+import Editor, { useMonaco } from "@monaco-editor/react";
 import '../Styles/Editor.css'
+import '../Styles/Utilities.css'
 import { useEffect } from 'react';
-
+import { RiArrowDropDownLine } from 'react-icons/ri'
+import { HiDotsVertical } from "react-icons/hi"
+import { IoMdMoon, IoMdSunny } from 'react-icons/io'
+import { SiC, SiCachet, SiCplusplus, SiJava, SiJavascript, SiPython } from 'react-icons/si'
 const defaultEditorConfig = {
-    theme: "vs-dark",
+    theme: "vs-light",
     value: "//Type something",
     fontSize: "20px",
     language: "Python",
-    height: "100vh",
+    height: "70vh",
     width: "400px"
 }
 
 function Editorcomponent() {
-    const [code, setCode] = useState("")
-    const [config, setConfig] = useState(defaultEditorConfig)
-    const editorconatinerref = useRef()
-    const middleelementref = useRef(null)
+    const monaco = useMonaco()
+    const [config, setEditorconfig] = useState(defaultEditorConfig)
+    const [theme, setTheme] = useState("green-black")
+    const [fontSize, setFontsize] = useState("18px")
+    const middelbarref = useRef(null)
+    const leftcontainerref = useRef(null)
     const rightcontainerref = useRef(null)
+    const [width, setWidth] = useState(0)
     const containerref = useRef(null)
-    const handleEditorchange = (value, event) => {
-        setCode(value)
-        console.log(value)
-    }
-
+    const [language, setLanguage] = useState("python")
     useEffect(() => {
         var drag = false
-        var moveX = editorconatinerref.current.getBoundingClientRect().width +
-            middleelementref.current.getBoundingClientRect().width / 2
-        middleelementref.current.addEventListener("mousedown", (e) => {
-            drag = true
-            moveX = e.x
-        })
-        containerref.current.addEventListener("mousemove", (e) => {
-            moveX = e.x
-            if (drag) {
-                editorconatinerref.current.style.width = moveX - middleelementref.current.getBoundingClientRect().width / 2 + "px"
+        var leftwidth = leftcontainerref.current.getBoundingClientRect().width
+        middelbarref.current.addEventListener("mousedown",
+            (e) => {
+                console.log(e.x)
+                drag = true
             }
-        })
-        containerref.current.addEventListener("mouseup", (e) => {
-            drag = false
-        })
+        )
+        containerref.current.addEventListener("mousemove",
+            (e) => {
+
+                if (drag) {
+                    console.log(e.x)
+                    setWidth(e.x)
+                    leftcontainerref.current.style.width = leftwidth+100+e.x + "px"
+
+                }
+            }
+        )
+        containerref.current.addEventListener("mouseup",
+            () => {
+                drag = false
+            }
+        )
+
+
+
+    }, [])
+    useEffect(() => {
+
     }, [])
     return (
-        <div ref={containerref}>
-            <div className='editorcontainer' ref={editorconatinerref}>
+        <div className='editor-container' ref={containerref} >
+            <div className='left-container-editor' ref={leftcontainerref}>
+                <div>
+                    this{width}
+                </div>
+            </div>
+            <div className='middle-bar-editor' ref={middelbarref}>
+                <HiDotsVertical size={20} color="white" />
+            </div>
+            <div className='right-container-editor' ref={rightcontainerref}>
+                <div className='accessory-btns'>
+                    <div class="dropdown">
+                        <button class="dropbtn">
+                            <div>
+                                Language
+                            </div>
+                            <RiArrowDropDownLine size={30} color="white" />
+                        </button>
+                        <div class="dropdown-content">
+                            <a onClick={() => setLanguage("python")}>
+                                <SiPython size={20} />
+                                Python</a>
+                            <a onClick={() => setLanguage("java")}>
+                                <SiJava size={20} />
+                                Java</a>
+                            <a onClick={() => setLanguage("c")}>
+                                <SiC size={20} />
+                                C</a>
+                            <a onClick={() => setLanguage("cpp")}>
+                                <SiCplusplus size={20} />
+                                C++</a>
+                            <a onClick={() => setLanguage("javascript")}>
+                                <SiJavascript size={20} />
+                                Javascript</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button class="dropbtn">
+                            <div>
+                                Theme
+                            </div>
+                            <RiArrowDropDownLine size={30} color="white" />
+                        </button>
+                        <div class="dropdown-content">
+                            <a onClick={() => setTheme("vs-light")}>
+                                <IoMdSunny size={20} />
+                                Light</a>
+                            <a onClick={() => setTheme("vs-dark")}>
+                                <IoMdMoon size={20} />
+                                Dark</a>
+                        </div>
+                    </div>
+                    <div class="dropdown">
+                        <button className='dropbtn'>
+                            <div>
+                                Font Size
+                            </div>
+                            <RiArrowDropDownLine size={30} color="white" />
+                        </button>
+                        <div class="dropdown-content">
+                            <a onClick={() => setFontsize("14px")}>14 px</a>
+                            <a onClick={() => setFontsize("16px")}>16 px</a>
+                            <a onClick={() => setFontsize("18px")}>18 px</a>
+                            <a onClick={() => setFontsize("20px")}>20 px</a>
+                            <a onClick={() => setFontsize("22px")}>22 px</a>
+                            <a onClick={() => setFontsize("24px")}>24 px</a>
+                        </div>
+                    </div>
+
+                </div>
                 <Editor
                     height={config.height}
                     defaultValue={config.value}
-                    theme={config.theme}
-                    language={config.language}
-                    onChange={handleEditorchange}
+                    theme={theme}
+                    language={language}
                     options={{
                         scrollBeyondLastLine: false,
-                        fontSize: "18px"
+                        fontSize: fontSize
                     }}
                 />
-                <div className='middle-cursorcontainer' ref={middleelementref}></div>
-                <div className='right-editorcontainer'>
+                <div>
+                    Naveenkumar
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
