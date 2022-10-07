c6451575317b30b7a8d93e1f8587bac271302fbc
Naveenkumar M <72449129+NaveenkumarMD@users.noreply.github.com>
2022-10-07 12:39:43 +0530

    Code editor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,21 @@
 import react from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Editor from './Components/Editor'
-import DB from './Components/DB'
+import DB from './Components/Collabcode'
 import Socket from './Components/Socket'
 import Login from './Screens/Login'
-function App(){
-  return(
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Editor/>} />
-          <Route path='/socket' element={<Socket/>} />
-          <Route path='/db' element={<DB/>} />
-          <Route path="/login" element={<Login/>}/>
-          
-        </Routes>
-      </BrowserRouter>
+import Editorpage from './Screens/Editorpage'
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Editor />} />
+        <Route path='/socket' element={<Socket />} />
+        <Route path='/db' element={<DB />} />
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
