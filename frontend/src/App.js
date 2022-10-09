358967011be3c20460ad109346c4e52f0fca9bbf
SanthoshM25 <santhoshm2504@gmail.com>
2022-10-09 19:00:02 +0530

    Fix: Merge conflict App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,15 +5,31 @@ import DB from "./Components/Collabcode";
 import Socket from "./Components/Socket";
 import Login from "./Screens/Login";
 import Editorpage from "./Screens/Editorpage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import UserContext from "./Context/UserContext";
+
 function App() {
+  const [userData, setUserData] = useState({
+    userName: "",
+    email: "",
+  });
+
+  const updateUserData = (newuserData) => {
+    setUserData({ ...userData, ...newuserData });
+  };
+
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Editor />} />
-        <Route path="/socket" element={<Socket />} />
-        <Route path="/db" element={<DB />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <UserContext.Provider value={{ userData, updateUserData }}>
+        <Routes>
+          <Route path="/" element={<Editor />} />
+          <Route path="/socket" element={<Socket />} />
+          <Route path="/db" element={<DB />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <ToastContainer autoClose={4000} />
+      </UserContext.Provider>
     </BrowserRouter>
   );
 }
