4cf5b20616e0bf011c53105f0872a11ad60df735
Santhosh <54148035+SanthoshM25@users.noreply.github.com>
2022-12-14 17:01:11 +0530

    Add: youtube api

---
+++ Backend/Src/Routes/YoutubeSearch.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+const youtubesearchapi = require("youtube-search-api");
+
+router.post("/search", (req, res) => {
+  youtubesearchapi
+    .GetListByKeyword(req.body.searchTerms, false, 5)
+    .then((result) => res.json(result))
+    .then((err) => console.log(err));
+});
+
+router.post("/video", (req, res) => {
+  youtubesearchapi
+    .GetVideoDetails(req.body.videoId)
+    .then((result) => res.json(result))
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
--- Backend/index.js
+++ Backend/index.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 //------------------------------------------------------
 app.use(require("./Src/Routes/index"));
 app.use(require("./Src/Routes/Runcode"));
+app.use(require("./Src/Routes/YoutubeSearch"));
 
 const server = app.listen(PORT, () => {
   console.log("App is running at", PORT);
